080339ef13beb81736fac9fdb309f8b16191a260
Joshua Kemp <josh.kemp@ziplist.com>
2014-06-21 19:56:13 -0400

    Changing scraper code to open uri

--- scraper.rb
+++ scraper.rb
@@ -1,21 +1,19 @@
-require "mechanize"
-require "nokogiri"
+require 'rubygems'
+require 'open-uri'
 
-url = ARGV[0]
-fp = File.new("wikilinks.txt", "w")
+@url = "http://www.igvita.com"
+@response = ''
 
-agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
+# open-uri RDoc: http://stdlib.rubyonrails.org/libdoc/open-uri/rdoc/index.html
+open(@url, "User-Agent" => "Ruby/#{RUBY_VERSION}",
+    "From" => "email@addr.com",
+    "Referer" => "http://www.igvita.com/") { |f|
+    puts "Fetched document: #{f.base_uri}"
+    puts "\t Content Type: #{f.content_type}\n"
+    puts "\t Charset: #{f.charset}\n"
+    puts "\t Content-Encoding: #{f.content_encoding}\n"
+    puts "\t Last Modified: #{f.last_modified}\n\n"
 
-html = agent.get(url).body
-
-html_doc = Nokogiri::HTML(html)
-
-fp.write("References\n\n")
-
-list = html_doc.xpath("//ol[@class='references']")
-list.each { |i| fp.write(i.text + "\n") }
-
-fp.write("Further Reading\n\n")
-
-list = html_doc.xpath("//span[@class='citation']")
-list.each { |i| fp.write(i.text + "\n") }
+    # Save the response body
+    @response = f.read
+}
