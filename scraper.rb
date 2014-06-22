9aa584601f892dc2802138e20096b99d431ee3c2
Michael Alger <mike.t.alger@gmail.com>
2014-06-21 20:55:03 -0400

    Initial login script

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,19 @@
 require "net/http"
 require "uri"
 
-uri = URI.parse("http://hyperiums.com/")
+uname = ARGV[0]
+pass = ARGV[1]
+
+uri = URI.parse("http://www.hyperiums.com/servlet/Login?login=#{uname}&lang=0&pwd=#{pass}")
 
 # Shortcut
-response = Net::HTTP.get_response(uri)
+response = Net::HTTP.get_print(uri)
+
+puts response
 
 # Will print response.body
-Net::HTTP.get_print(uri)
+#Net::HTTP.get_print(uri)
 
 # Full
-http = Net::HTTP.new(uri.host, uri.port)
-response = http.request(Net::HTTP::Get.new(uri.request_uri))
+#http = Net::HTTP.new(uri.host, uri.port)
+#response = http.request(Net::HTTP::Get.new(uri.request_uri))
