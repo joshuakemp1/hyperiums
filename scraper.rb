c2c1ea119bb1f1a5adda597ec394c90b6111be2e
Michael Alger <mike.t.alger@gmail.com>
2014-06-21 21:01:55 -0400

    update response processing

--- scraper.rb
+++ scraper.rb
@@ -4,16 +4,16 @@ require "uri"
 uname = ARGV[0]
 pass = ARGV[1]
 
-uri = URI.parse("http://www.hyperiums.com/servlet/Login?login=#{uname}&lang=0&pwd=#{pass}")
+uri = URI.parse("http://hyp2.hyperiums.com/servlet/Login?login=#{uname}&lang=0&pwd=#{pass}")
 
 # Shortcut
-response = Net::HTTP.get_print(uri)
-
-puts response
+#response = Net::HTTP.get_print(uri)
 
 # Will print response.body
 #Net::HTTP.get_print(uri)
 
 # Full
-#http = Net::HTTP.new(uri.host, uri.port)
-#response = http.request(Net::HTTP::Get.new(uri.request_uri))
+http = Net::HTTP.new(uri.host, uri.port)
+response = http.request(Net::HTTP::Get.new(uri.request_uri))
+
+puts response
